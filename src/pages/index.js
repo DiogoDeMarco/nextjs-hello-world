0220fcf498358db554250904f586ccd20467e7c1
Diogo De Marco <diogotavaresdemarco@gmail.com>
2026-07-14 11:09:28 -0300

    Update index.js
    cd

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      Hello World.{" "}
+      Ovo de codorna.{" "}
       <Link href="/about">
         About
       </Link>
